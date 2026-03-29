43a53d8b645c786a4ff04ccf77c5f3472375d2ef
Payton <565524110@qq.com>
2026-03-29 23:44:25 +0800

    feat: 支持通过 cargo embed 烧录

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,22 @@
 #![no_std]
 #![no_main]
 
-// use panic_halt as _;
-
 use core::panic::PanicInfo;
 use cortex_m_rt::entry;
+use rtt_target::{rprintln, rtt_init_print};
 use stm32f1xx_hal::prelude::*;
 use stm32f1xx_hal::stm32 as device;
-use cortex_m_semihosting::hprintln;
 
 #[panic_handler]
 fn my_panic_handler(_panic: &PanicInfo) -> ! {
+    rprintln!("PANIC: 程序发生严重错误");
     loop {}
 }
 
 #[entry]
 fn main() -> ! {
+    rtt_init_print!();
+
     let dp = device::Peripherals::take().unwrap();
     let cp = cortex_m::Peripherals::take().unwrap();
     let mut flash = dp.FLASH.constrain();
@@ -25,12 +26,26 @@ fn main() -> ! {
     let mut pc13 = gpio_c.pc13.into_push_pull_output(&mut gpio_c.crh);
     let mut delay = cp.SYST.delay(&clocks);
 
-    hprintln!("hello world").unwrap();
+    rprintln!("=== STM32 RTT 示例程序 ===");
+    rprintln!("系统初始化完成，时钟频率：{} Hz", clocks.sysclk().to_Hz());
+    rprintln!("开始闪烁 LED...");
+
+    let mut counter: u32 = 0;
 
     loop {
-        pc13.set_low();
-        delay.delay_ms(1000u32);
         pc13.set_high();
+        rprintln!("[{}] LED ON", counter);
         delay.delay_ms(1000u32);
+
+        pc13.set_low();
+        rprintln!("[{}] LED OFF", counter);
+        delay.delay_ms(1000u32);
+
+        counter += 1;
+
+        // 每 10 次循环输出一个统计信息
+        if counter % 10 == 0 {
+            rprintln!("--- 已运行 {} 个周期 ---", counter);
+        }
     }
 }
