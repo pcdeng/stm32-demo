9872bc8aeb114848bf00fa434f064d79c5d9a2f4
Payton <565524110@qq.com>
2026-03-30 21:00:01 +0800

    fix: 增加备注

--- src/main.rs
+++ src/main.rs
@@ -4,8 +4,7 @@
 use core::panic::PanicInfo;
 use cortex_m_rt::entry;
 use rtt_target::{rprintln, rtt_init_print};
-use stm32f1xx_hal::prelude::*;
-use stm32f1xx_hal::stm32 as device;
+use stm32f1xx_hal::{prelude::*, stm32 as device};
 
 #[panic_handler]
 fn my_panic_handler(_panic: &PanicInfo) -> ! {
@@ -15,15 +14,21 @@ fn my_panic_handler(_panic: &PanicInfo) -> ! {
 
 #[entry]
 fn main() -> ! {
+    // 1. 初始化 RTT
     rtt_init_print!();
 
-    let dp = device::Peripherals::take().unwrap();
-    let cp = cortex_m::Peripherals::take().unwrap();
+    // 2. 获取外设
+    let cp = cortex_m::Peripherals::take().unwrap(); // 核心外设
+    let dp = device::Peripherals::take().unwrap(); // 设备外设
     let mut flash = dp.FLASH.constrain();
     let rcc = dp.RCC.constrain();
     let clocks = rcc.cfgr.freeze(&mut flash.acr);
+
+    // 3. 配置 GPIO
     let mut gpio_c = dp.GPIOC.split();
     let mut pc13 = gpio_c.pc13.into_push_pull_output(&mut gpio_c.crh);
+
+    // 4. 配置延时
     let mut delay = cp.SYST.delay(&clocks);
 
     rprintln!("=== STM32 RTT 示例程序 ===");
